4269e3eb1e1b968b5cc0468c868875f318688b36
mansourdalia17-dot <mansourdalia17@gmail.com>
2025-09-13 19:09:34 +0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task1_Dalia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
